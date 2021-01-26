4d143f4ffc04e6d13e201e6d14b61df81787bf20
inky <1530334435@qq.com>
2021-01-26 22:49:42 +0800

    Initial Commit

---
+++ miniprogram/pages/music/music.js
@@ -0,0 +1,92 @@
+// pages/music/music.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+   imgUrls:[{
+     url:'http://p1.music.126.net/nVUH7O5ZNMG1OQ1kE-tq9g==/109951165665595417.jpg?imageView&quality=89'
+   },
+     {
+      url:'http://p1.music.126.net/SLfispSeeEnb6Ezs0cNjBw==/109951165666128356.jpg?imageView&quality=89'
+     },
+      {
+        url:'http://p1.music.126.net/C9I9GxpvRX7nCZyXNBeqOw==/109951165664694558.jpg?imageView&quality=89'
+   }] ,
+   playlist:[{
+    "id":"1001",
+    "playCount":1.234244e+06,
+    "name":"让人心动的乐曲",
+    "picUrl":"http://p2.music.126.net/Xvo6PwBcdOA69ipcpV9YYg==/109951165463253777.jpg?param=140y140"
+   },
+   {
+    "id":"1002",
+    "playCount":1.234244e+06,
+    "name":"东方，永恒的幻想乡",
+    "picUrl":"https://p3.music.126.net/7sZ5WjNooQZ53-JHiTClkw==/6062707115744662.jpg?param=200y200"
+   },
+   {
+    "id":"1003",
+    "playCount":1.234244e+06,
+    "name":"此生无悔入东方，来世愿生幻想乡",
+    "picUrl":" https://p4.music.126.net/pfAn8ma5TfCge6Is1QeoDQ==/109951163582495756.jpg?param=200y200"
+ }]
+  },
+ 
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
